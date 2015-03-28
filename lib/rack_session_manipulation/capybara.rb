988394299c67694758a54f66db8a814d939eab7e
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-03-27 21:06:38 -0400

    Cleanup and a bit of insurance

--- lib/rack_session_manipulation/capybara.rb
+++ lib/rack_session_manipulation/capybara.rb
@@ -3,14 +3,17 @@ module RackSessionManipulation
   # Capybara helpers for accessing and setting session values
   module Capybara
     def session=(hash)
-      data = RackSessionManipulation.encode(hash)
-      page.driver.put(RackSessionManipulation.config.path, session_data: data)
+      data = { session_data: RackSessionManipulation.encode(hash) }
+
+      page.driver.put(RackSessionManipulation.config.path, data)
+      page.driver.status_code.should eql(204)
     end
 
     def session
       page.driver.get(RackSessionManipulation.config.path)
-      data = page.driver.last_response.body
-      RackSessionManipulation.decode(data)
+      page.driver.status_code.should eql(200)
+
+      RackSessionManipulation.decode(page.driver.last_response.body)
     end
   end
 end
