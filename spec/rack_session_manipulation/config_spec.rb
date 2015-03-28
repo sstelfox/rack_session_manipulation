b45fff71ea89d8b969bc23237030ee02c2314294
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-03-27 23:53:43 -0400

    Fixed config style

--- spec/rack_session_manipulation/config_spec.rb
+++ spec/rack_session_manipulation/config_spec.rb
@@ -1,20 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe(RackSessionManipulation::Config) do
-  subject { RackSessionManipulation::Config }
-  let(:instance) { subject.new }
-
   it 'defaults to a nil path' do
-    expect(instance.path).to eq(nil)
+    expect(subject.path).to eq(nil)
   end
 
   it 'allows initializing with a value' do
-    inst = subject.new('value')
+    inst = described_class.new('value')
     expect(inst.path).to eq('value')
   end
 
   it 'allows resetting the value after initialization' do
-    instance.path = 'else'
-    expect(instance.path).to eq('else')
+    subject.path = 'else'
+    expect(subject.path).to eq('else')
   end
 end
