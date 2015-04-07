6dd277bc86977987b9dfe9ed8600591fdf93a6a5
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-04-07 10:39:31 -0400

    More tests

--- spec/rack_session_manipulation/middleware_spec.rb
+++ spec/rack_session_manipulation/middleware_spec.rb
@@ -4,37 +4,44 @@ RSpec.describe(RackSessionManipulation::Middleware) do
   let(:app)             { double }
   let(:default_config)  { RackSessionManipulation::Config.new }
 
-  let(:app_env) do
-    {
-      'PATH_INFO' => '/users/1',
-      'REQUEST_METHOD' => 'GET'
-    }
-  end
-
-  let(:app_env2) do
-    {
-      'PATH_INFO' => default_config.path,
-      'REQUEST_METHOD' => 'POST',
-      'rack.input' => StringIO.new('')
-    }
-  end
-
-  let(:rsm_env) do
-    {
-      'PATH_INFO' => default_config.path,
-      'REQUEST_METHOD' => 'GET'
-    }
-  end
-
   subject { described_class.new(app) }
 
-  it 'falls back to the normal app when path doesn\'t match' do
-    expect(app).to receive(:call).with(app_env)
-    subject.call(app_env)
+  context 'Original application' do
+    it 'falls back to the normal app when path doesn\'t match' do
+      env = Rack::MockRequest.env_for('/users/1')
+      expect(app).to receive(:call).with(env)
+
+      subject.call(env)
+    end
+
+    it 'falls back to the normal app when method doesn\'t match' do
+      env = Rack::MockRequest.env_for(default_config.path, method: :post)
+      expect(app).to receive(:call).with(env)
+
+      subject.call(env)
+    end
   end
 
-  it 'falls back to the normal app when method doesn\'t match' do
-    expect(app).to receive(:call).with(app_env2)
-    subject.call(app_env2)
+  context 'RSM Middleware' do
+    it 'calls the reset method on a DELETE request' do
+      env = Rack::MockRequest.env_for(default_config.path, method: :delete)
+      expect(subject).to receive(:reset)
+
+      subject.call(env)
+    end
+
+    it 'calls the retrieve method on a GET request' do
+      env = Rack::MockRequest.env_for(default_config.path, method: :get)
+      expect(subject).to receive(:retrieve)
+
+      subject.call(env)
+    end
+
+    it 'calls the update method on a PUT request' do
+      env = Rack::MockRequest.env_for(default_config.path, method: :put)
+      expect(subject).to receive(:update)
+
+      subject.call(env)
+    end
   end
 end
